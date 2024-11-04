b1f3031995ec014f5a891808a12d139da4be5943
mfeti <mfeti2175@gmail.com>
2024-11-04 06:52:02 +0300

    setup generic image access

--- src/assets/website/index.js
+++ src/assets/website/index.js
@@ -1,4 +1,5 @@
 import DarkButton from "./dark-mode-button.png";
 import LightButton from "./light-mode-button.png";
-
-export { DarkButton, LightButton };
+import OrangePattern from "./orange-pattern.jpg";
+import FooterPattern from "./footer-pattern.jpg";
+export { DarkButton, LightButton, OrangePattern, FooterPattern };
