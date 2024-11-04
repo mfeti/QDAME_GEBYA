6251b658dce132a78e7eebbaa74895816dd9c8fc
mfeti <mfeti2175@gmail.com>
2024-11-04 07:08:05 +0300

    Subscribe animation done

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -10,15 +10,21 @@ const bgImage = {
 function Subscribe() {
   return (
     <section
+      data-aos="zoom-in"
       style={bgImage}
       className="min-h-[200px] flex justify-center items-center"
     >
       <div className="container">
-        <div className="space-y-4">
-          <h2 className="text-3xl sm:text-4xl font-bold text-white">
+        <div className="space-y-4 max-w-[600px] mx-auto">
+          <h2
+            data-aos="fade-up"
+            className="text-3xl sm:text-4xl font-bold text-white"
+          >
             Get Notified About New Products
           </h2>
           <input
+            data-aos="fade-up"
+            data-aos-delay="200"
             type="email"
             placeholder="Enter your email"
             className="w-full p-3 text-[#222] "
