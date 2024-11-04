40871c564a9d05f5aca675c46e3c79178a432a9a
mfeti <mfeti2175@gmail.com>
2024-11-04 06:52:22 +0300

    set up constants image

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,9 +2,14 @@ import Img1 from "../assets/women/women.png";
 import Img2 from "../assets/women/women2.jpg";
 import Img3 from "../assets/women/women3.jpg";
 import Img4 from "../assets/women/women4.jpg";
+
 import Image1 from "../assets/hero/women.png";
 import Image2 from "../assets/hero/shopping.png";
 import Image3 from "../assets/hero/sale.png";
+
+import ImgS1 from "../assets/shirt/shirt.png";
+import ImgS2 from "../assets/shirt/shirt2.png";
+import ImgS3 from "../assets/shirt/shirt3.png";
 
 export const NavLinks = [
   {
@@ -116,5 +121,75 @@ export const ProductsData = [
     rating: 4.5,
     color: "Pink",
     aosDelay: "800",
+  },
+];
+
+export const TopProductsData = [
+  {
+    id: 1,
+    img: ImgS1,
+    title: "Casual Wear",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 2,
+    img: ImgS2,
+    title: "Printed shirt",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: 3,
+    img: ImgS3,
+    title: "Women shirt",
+    description:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+export const TestimonialData = [
+  {
+    id: 1,
+    name: "Victor Mos",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 2,
+    name: "Mohammed A.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 3,
+    name: "Halima A.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/104/104",
+  },
+  {
+    id: 5,
+    name: "John Doe",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/103/103",
+  },
+];
+
+export const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
   },
 ];
